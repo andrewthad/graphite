4f01743eca5ae2b6a220d81db9f620f39dffb570
Andrew Martin <amartin@layer3com.com>
2016-03-03 17:03:46 -0500

    major refactor - sort of working now

--- example/main.hs
+++ example/main.hs
@@ -12,20 +12,22 @@ import           Diagrams.Prelude             hiding ((:&))
 import           StackedBarGraph
 
 main :: IO ()
-main = mainWith $
-  ( build myGraphA funcData
-    ===
-    axisX 16 (map show allAges)
-    ===
-    square 4
-  ) |||
-  ( axisY 16 ["1","2","3","4","5"] ||| build myGraphB funcData) |||
-  ( build myGraphD funcDataGender
-    ===
-    axisX 16 (map show allGenders)
-    ===
-    square 4
-  )
+-- main = mainWith $ showOrigin $ runGraphite myGraphH fromIntegral funcDataGender (allGenders :& RNil) ()
+main = mainWith $ showOrigin $ runGraphite myGraphI fromIntegral funcData2 (allAges :& allGenders :& RNil) ()
+
+-- ( build myGraphA funcData
+--   ===
+--   axisX 16 (map show allAges)
+--   ===
+--   square 4
+-- ) |||
+-- ( axisY 16 ["1","2","3","4","5"] ||| build myGraphB funcData) |||
+-- ( build myGraphD funcDataGender
+--   ===
+--   axisX 16 (map show allGenders)
+--   ===
+--   square 4
+-- )
 
 data Gender = Male | Female | Unknown
   deriving (Eq,Ord,Bounded,Enum,Show)
@@ -38,27 +40,46 @@ allAges = enumFromTo minBound maxBound
 allGenders :: [Gender]
 allGenders = enumFromTo minBound maxBound
 
-myGraphA,myGraphB :: Graphite '[Age,Gender] Int
-myGraphA = intervals 16 allAges $ grouped 1 allGenders $ bar fromIntegral 4 id
-myGraphB = id
-  $ intervalsWithAxis 16 show (zip allAges (repeat id))
-  -- $ stacked allGenders
-  $ stackedWith [ (Male,fc purple . atop (text "Male" & fontSizeL 2 & fc white))
-                , (Female, fc blue . atop (text "Female" & fontSizeL 2 & fc white))
-                , (Unknown, fc green . atop (text "N/A" & fontSizeL 2 & fc white))
-                ]
-  $ bar fromIntegral 6.0 id
+myGraphE :: Graphite (Diagram B) r '[Gender] Int
+myGraphE = stacked id $ bar 4.0
 
-myGraphC :: Graphite '[Gender] Int
-myGraphC = intervals 16 allGenders $ bar fromIntegral 4.0 (fc blue)
+myGraphF :: Graphite (Diagram B) r '[Age,Gender] Int
+myGraphF = paddedBy 12.0 id $ stacked id $ barColoured 4.0 genderColour
 
-myGraphD :: Graphite '[Gender] Int
-myGraphD = lineGraph fromIntegral 16.0 allGenders (fc blue)
+myGraphH :: Graphite (Diagram B) r '[Gender] Int
+myGraphH = connected 4 $ gvalue
+
+myGraphI :: Graphite (Diagram B) r '[Age,Gender] Int
+myGraphI = overlapped $ connected 16.0 $ gvalue
+
+
+genderColour :: Gender -> Colour Double
+genderColour g = case g of
+  Male -> blue
+  Female -> magenta
+  Unknown -> grey
+
+-- myGraphA,myGraphB :: Graphite '[Age,Gender] Int
+-- myGraphA = intervals 16 allAges $ grouped 1 allGenders $ bar fromIntegral 4 id
+-- myGraphB = id
+--   $ intervalsWithAxis 16 show (zip allAges (repeat id))
+--   -- $ stacked allGenders
+--   $ stackedWith [ (Male,fc purple . atop (text "Male" & fontSizeL 2 & fc white))
+--                 , (Female, fc blue . atop (text "Female" & fontSizeL 2 & fc white))
+--                 , (Unknown, fc green . atop (text "N/A" & fontSizeL 2 & fc white))
+--                 ]
+--   $ bar fromIntegral 6.0 id
+--
+-- myGraphC :: Graphite '[Gender] Int
+-- myGraphC = intervals 16 allGenders $ bar fromIntegral 4.0 (fc blue)
+--
+-- myGraphD :: Graphite '[Gender] Int
+-- myGraphD = lineGraph fromIntegral 16.0 allGenders (fc blue)
 
 funcDataGender :: Rec Identity '[Gender] -> Int
 funcDataGender (Identity gender :& RNil) = case gender of
-  Male -> 11
-  Female -> 14
+  Male    -> 11
+  Female  -> 14
   Unknown -> 2
 
 funcData :: Rec Identity '[Age,Gender] -> Int
@@ -70,6 +91,21 @@ funcData (Identity age :& Identity gender :& RNil) = fAge age * fGender gender
   fGender Male   = 4
   fGender Female = 5
   fGender Unknown = 1
+
+funcData2 :: Rec Identity '[Age,Gender] -> Int
+funcData2 (Identity age :& Identity gender :& RNil) = case age of
+  Young -> case gender of
+    Male -> 18
+    Female -> 16
+    Unknown -> 30
+  Middle -> case gender of
+    Male -> 12
+    Female -> 18
+    Unknown -> 22
+  Old -> case gender of
+    Male -> 2
+    Female -> 26
+    Unknown -> 20
 
 
 
--- src/StackedBarGraph.hs
+++ src/StackedBarGraph.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module StackedBarGraph where
 
@@ -25,171 +27,225 @@ import           Text.Blaze.Html               (Html, preEscapedToHtml)
 -- (Rec Identity rs -> Double)
 -- newtype Grande a = Grande (a -> Diagram B -> Diagram B)
 
-newtype Graphite rs a = Graphite ((Rec Identity rs -> a) -> Diagram B)
+-- type family Head xs where
+--   Head (x ': xs) = x
+--
+-- type family Tail xs where
+--   Tail (x ': xs) = xs
 
-modifyGraphite :: ([Diagram B] -> Diagram B) -> [r] -> Graphite rs a -> Graphite (r ': rs) a
-modifyGraphite combine vals (Graphite g) = Graphite $ \getData ->
-  combine $ flip map vals $ \val -> g (curryRecFunc getData (Identity val))
-
-modifyGraphiteWith :: ([Diagram B] -> Diagram B) -> [(r, Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
-modifyGraphiteWith combine vals (Graphite g) = Graphite $ \getData ->
-  combine $ flip map vals $ \(val,edit) -> g (curryRecFunc getData (Identity val)) & edit
-
-bar :: (a -> Double) -> Double -> (Diagram B -> Diagram B) -> Graphite '[] a
-bar f width mods = Graphite (\g -> rect width (f (g RNil)) & mods)
-
-lineGraph :: (a -> Double) -> Double -> [r] -> (Diagram B -> Diagram B) -> Graphite '[r] a
-lineGraph f spacing vals mods = Graphite $ \g -> id
-  $ fromVertices
-  $ map (\(x,y) -> P $ V2 x y)
-  $ zip (enumFromThen 0 spacing)
-  $ flip map vals $ \val -> f (g (Identity val :& RNil))
-
--- The numeric argument refers to the amount of space
--- between each bar.
-grouped :: Double -> [r] -> Graphite rs a -> Graphite (r ': rs) a
-grouped innerSpacing vals (Graphite g) = Graphite $ \getData ->
-  hsep innerSpacing $ flip map vals $ \val -> alignB $ g (curryRecFunc getData (Identity val))
-
-stacked :: [r] -> Graphite rs a -> Graphite (r ': rs) a
-stacked = modifyGraphite (alignB . vcat)
-
--- stackedB :: [r] -> Graphite (r ': rs) a -> Graphite (r ': rs) a
-
--- stacked :: [r] -> Graphite rs a -> Graphite (r ': rs) a
--- stacked vals (Graphite g) = Graphite $ \getData ->
---   alignB $ vcat $ flip map vals $ \val -> g (curryRecFunc getData (Identity val))
-
-stackedWith :: [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
-stackedWith vals (Graphite g) = Graphite $ \getData ->
-  alignB $ vcat $ flip map vals $ \(val,edit) -> g (curryRecFunc getData (Identity val)) & edit
-
--- We don't actually want to use hcat. We need to space everything
--- by a constant amount, and there should be some extra space on the left and right.
-intervals :: Double -> [r] -> Graphite rs a -> Graphite (r ': rs) a
-intervals spacing vals (Graphite g) = Graphite $ \getData ->
-  atPoints (intervalPoints spacing (length vals))
-  $ surroundWith mempty $ flip map vals $ \val -> alignB $ g (curryRecFunc getData (Identity val))
-
-intervalsWith :: Double -> [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
-intervalsWith spacing = modifyGraphiteWith
-  (\diagrams -> atPoints (intervalPoints spacing (length diagrams)) (surroundWith mempty diagrams))
-
-intervalsWithAxis :: Double -> (r -> String) -> [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
-intervalsWithAxis spacing toLabel vals g = modifyGraphiteWith (\diagrams ->
-  atPoints (intervalPoints spacing (length diagrams)) (surroundWith mempty diagrams)
-  ===
-  axisX spacing (map (toLabel . fst) vals)
-  ) vals g
-
-build :: Graphite rs a -> (Rec Identity rs -> a) -> Diagram B
-build (Graphite f) g = f g
-
-axisX :: Double -> [String] -> Diagram B
-axisX interval strs =
-  fromOffsets [scaleX (interval * fromIntegral (length strs)) unitX]
-  ===
-  atPoints (intervalPoints interval (length strs))
-    (surroundWith mempty $ map (\str -> alignedText 0.5 1.0 str # fontSizeL 2) strs)
-
-axisY :: Double -> [String] -> Diagram B
-axisY interval strs =
-  fromOffsets [scaleY (interval * fromIntegral (length strs)) unitY]
-  |||
-  atPoints (intervalPointsY interval (length strs))
-    ((mempty:) $ map (\str -> alignedText 1.0 0.5 str # fontSizeL 2) strs)
-
-intervalPointsY :: Double -> Int -> [Point V2 Double]
-intervalPointsY spacing n =
-  (fromOffsets $ (scaleY spacing unitY :) $
-    (replicate n (scaleY spacing unitY)))
-
-intervalPoints :: Double -> Int -> [Point V2 Double]
-intervalPoints spacing n =
-  (fromOffsets $ surroundWith (scaleX (spacing / 2) unitX) $
-    (replicate n (scaleX spacing unitX)))
-
-increasingX :: Double -> [Point V2 Double]
-increasingX inc = go 0
-  where
-  go n = scaleX n unitX : go (n + inc)
-
-surroundWith :: a -> [a] -> [a]
-surroundWith a as = [a] ++ as ++ [a]
-
--- Move this into vinyl-plus
--- Can we show that
--- (Rec f (a ': as) -> b) -> f a -> Rec f as -> b
--- its like curry for records
-curryRecFunc :: (Rec f (a ': as) -> b) -> f a -> Rec f as -> b
-curryRecFunc f r rs = f (r :& rs)
-
-renderStackedBarGraph :: StackedBarGraphSettings -> [(String,[Int])] -> Html
-renderStackedBarGraph bgs pairs = preEscapedToHtml . renderText
-  $ renderDia SVG (SVGOptions (mkWidth 400) Nothing "bargraph")
-  $ buildStackedBarGraph bgs pairs
-
-buildStackedBarGraph :: StackedBarGraphSettings -> [(String,[Int])] -> Diagram B
-buildStackedBarGraph bgs pairs =
-  let maxY = fromMaybe 100 $ maximumMay $ map (sum . snd) pairs
-      (yInterval, intervalCount) = chooseOptimalInterval maxY
-      yIntervalHeight = bgsHeight bgs / (fromIntegral intervalCount + 0.3)
-      scaleUnit i = fromIntegral i * yIntervalHeight / fromIntegral yInterval
-  in buildYAxis bgs yIntervalHeight intervalCount yInterval # alignBR
-     <> buildStackedContent bgs scaleUnit pairs # alignL
-
-chooseOptimalInterval :: Int -> (Int,Int)
-chooseOptimalInterval maxY =
-  fromMaybe (1000000,8)
-  $ fmap (\(interval',ct,_) -> (interval',ct))
-  $ minimumByMay (\(_,_,a) (_,_,b) -> compare a b)
-  $ do
-    x <- acceptableIntervals
-    y <- acceptableCounts
-    let diff' = x * y - maxY
-    guard $ diff' >= 0
-    return (x,y,diff')
-  where acceptableIntervals = [1,2,3,4,5,10,20,50,100,200,500,1000,10000,100000]
-        acceptableCounts = [4,5,6,7]
-
-
-buildYAxis :: StackedBarGraphSettings -> Double -> Int -> Int -> Diagram B
-buildYAxis bgs yIntervalHeight yIntervalCount i =
-  let nums = reverse $ take yIntervalCount $ map (*i) [1..]
-      -- shiftCenter x = translate (r2 (0, x))
-      ticks = foldl' (===) mempty $ flip map nums $ \n ->
-        hsep 3 [ alignedText 1 0.5 (show n) # fontSizeL (bgsAxisFontSize bgs)
-               , hrule (bgsTickSize bgs)
-               ] <> rect (bgsIntervalSpace bgs) yIntervalHeight # lw none # alignTR
-  in hcat $ map alignB $
-     [ ticks
-     , vrule (bgsHeight bgs)
-     ]
-
-buildStackedContent :: StackedBarGraphSettings -> (Int -> Double) -> [(String,[Int])] -> Diagram B
-buildStackedContent bgs scaleUnit pairs =
-  let pairCount = length pairs
-      pieceWidth = (bgsWidth bgs) / fromIntegral pairCount
-      barWidth = pieceWidth * 0.9
-      infiniteColors = cycle $ bgsColors bgs
-      mkPiece label vals = mempty
-        === vcat (reverse $ flip map (zip vals infiniteColors) $ \(val,color) ->
-                  rect barWidth (scaleUnit val) # fc color # lw none) # alignB
-        === hrule pieceWidth
-        === alignedText 0.5 1.0 label # fontSizeL (bgsAxisFontSize bgs)
-  in hcat (map (uncurry mkPiece) pairs) === rect (bgsAxisFontSize bgs) (bgsAxisFontSize bgs) # lw none
-
-data StackedBarGraphSettings = StackedBarGraphSettings
-  { bgsColors        :: [Colour Double]
-  , bgsTickSize      :: Double
-  , bgsAxisFontSize  :: Double
-  , bgsHeight        :: Double
-  , bgsWidth         :: Double
-  , bgsIntervalSpace :: Double
+newtype Graphite b r rs a = Graphite
+  { runGraphite
+    :: (a -> Double)
+    -> (Rec Identity rs -> a)
+    -> Rec [] rs
+    -> r
+    -> b
   }
 
-defStackedBarGraphSettings :: StackedBarGraphSettings
-defStackedBarGraphSettings = StackedBarGraphSettings
-  (brewerSet Set1 8)
-  5 16 200 400 40
+-- modifyGraphite :: ([Diagram B] -> Diagram B) -> [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- modifyGraphite combine vals (Graphite g) = Graphite $ \getData ->
+--   combine $ flip map vals $ \val -> g (curryRecFunc getData (Identity val))
+--
+-- modifyGraphiteWith :: ([Diagram B] -> Diagram B) -> [(r, Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
+-- modifyGraphiteWith combine vals (Graphite g) = Graphite $ \getData ->
+--   combine $ flip map vals $ \(val,edit) -> g (curryRecFunc getData (Identity val)) & edit
+
+single :: (r -> a -> m) -> Graphite m r '[] a
+single f = Graphite (\fromVal getVal RNil r -> f r (getVal RNil))
+
+gvalue :: Graphite Double r '[] a
+gvalue = Graphite (\fromVal getVal RNil _ -> fromVal (getVal RNil))
+
+bar :: Double -> Graphite (Diagram B) r '[] a
+bar width = Graphite (\fromVal getVal RNil _ -> rect width (fromVal (getVal RNil)))
+
+barColoured :: Double -> (r -> Colour Double) -> Graphite (Diagram B) r '[] a
+barColoured width getColour =
+  Graphite (\fromVal getVal RNil r -> rect width (fromVal (getVal RNil)) & fc (getColour r))
+
+-- stacked :: [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- stacked = modifyGraphite (alignB . vcat)
+
+stacked :: (m -> Diagram B) -> Graphite m r rs a -> Graphite (Diagram B) s (r ': rs) a
+stacked f (Graphite g) = Graphite $ \fromVal getVal (vals :& valsNext) s ->
+  alignB $ vcat $ flip map vals $ \r ->
+    f $ g fromVal (curryRecFunc getVal (Identity r)) valsNext r
+
+paddedBy :: Double -> (m -> Diagram B) -> Graphite m r rs a -> Graphite (Diagram B) s (r ': rs) a
+paddedBy spacing f (Graphite g) = Graphite $ \fromVal getVal (vals :& valsNext) s ->
+  hsep spacing $ flip map vals $ \r ->
+    f $ g fromVal (curryRecFunc getVal (Identity r)) valsNext r
+
+connected :: Double -> Graphite Double r rs a -> Graphite (Diagram B) s (r ': rs) a
+connected spacing (Graphite g) = Graphite $ \fromVal getVal (vals :& valsNext) s -> id
+  $ fromVertices
+  $ flip map (zip [0,1..] vals)
+  $ \(i :: Int,r) -> P $ V2
+        (spacing * fromIntegral i)
+        (g fromVal (curryRecFunc getVal (Identity r)) valsNext r)
+
+overlapped :: Graphite (Diagram B) r rs a -> Graphite (Diagram B) s (r ': rs) a
+overlapped (Graphite g) = Graphite $ \fromVal getVal (vals :& valsNext) s ->
+  mconcat $ flip map vals $ \r -> g fromVal (curryRecFunc getVal (Identity r)) valsNext r
+
+-- consConcat :: ([m] -> Diagram B) -> Graphite m r rs a -> Graphite (Diagram B) s (r ': rs) a
+-- consConcat f (Graphite g) = Graphite $ \fromVal getVal (vals :& valsNext) s ->
+--   f $ flip map vals $ \r -> g fromVal (curryRecFunc getVal (Identity r)) valsNext r
+
+
+-- -- We don't actually want to use hcat. We need to space everything
+-- -- by a constant amount, and there should be some extra space on the left and right.
+-- intervals :: Double -> [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- intervals spacing vals (Graphite g) = Graphite $ \getData ->
+--   atPoints (intervalPoints spacing (length vals))
+--   $ surroundWith mempty $ flip map vals $ \val -> alignB $ g (curryRecFunc getData (Identity val))
+--
+-- intervalsWith :: Double -> [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
+-- intervalsWith spacing = modifyGraphiteWith
+--   (\diagrams -> atPoints (intervalPoints spacing (length diagrams)) (surroundWith mempty diagrams))
+--
+-- intervalsWithAxis :: Double -> (r -> String) -> [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
+-- intervalsWithAxis spacing toLabel vals g = modifyGraphiteWith (\diagrams ->
+--   atPoints (intervalPoints spacing (length diagrams)) (surroundWith mempty diagrams)
+--   ===
+--   axisX spacing (map (toLabel . fst) vals)
+--   ) vals g
+--
+-- lineGraph :: (a -> Double) -> Double -> [r] -> (Diagram B -> Diagram B) -> Graphite '[r] a
+-- lineGraph f spacing vals mods = Graphite $ \g -> id
+--   $ fromVertices
+--   $ map (\(x,y) -> P $ V2 x y)
+--   $ zip (enumFromThen 0 spacing)
+--   $ flip map vals $ \val -> f (g (Identity val :& RNil))
+--
+-- -- The numeric argument refers to the amount of space
+-- -- between each bar.
+-- grouped :: Double -> [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- grouped innerSpacing vals (Graphite g) = Graphite $ \getData ->
+--   hsep innerSpacing $ flip map vals $ \val -> alignB $ g (curryRecFunc getData (Identity val))
+--
+-- stacked :: [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- stacked = modifyGraphite (alignB . vcat)
+--
+-- -- stackedB :: [r] -> Graphite (r ': rs) a -> Graphite (r ': rs) a
+--
+-- -- stacked :: [r] -> Graphite rs a -> Graphite (r ': rs) a
+-- -- stacked vals (Graphite g) = Graphite $ \getData ->
+-- --   alignB $ vcat $ flip map vals $ \val -> g (curryRecFunc getData (Identity val))
+--
+-- stackedWith :: [(r,Diagram B -> Diagram B)] -> Graphite rs a -> Graphite (r ': rs) a
+-- stackedWith vals (Graphite g) = Graphite $ \getData ->
+--   alignB $ vcat $ flip map vals $ \(val,edit) -> g (curryRecFunc getData (Identity val)) & edit
+--
+--
+-- build :: Graphite rs a -> (Rec Identity rs -> a) -> Diagram B
+-- build (Graphite f) g = f g
+--
+-- axisX :: Double -> [String] -> Diagram B
+-- axisX interval strs =
+--   fromOffsets [scaleX (interval * fromIntegral (length strs)) unitX]
+--   ===
+--   atPoints (intervalPoints interval (length strs))
+--     (surroundWith mempty $ map (\str -> alignedText 0.5 1.0 str # fontSizeL 2) strs)
+--
+-- axisY :: Double -> [String] -> Diagram B
+-- axisY interval strs =
+--   fromOffsets [scaleY (interval * fromIntegral (length strs)) unitY]
+--   |||
+--   atPoints (intervalPointsY interval (length strs))
+--     ((mempty:) $ map (\str -> alignedText 1.0 0.5 str # fontSizeL 2) strs)
+--
+-- intervalPointsY :: Double -> Int -> [Point V2 Double]
+-- intervalPointsY spacing n =
+--   (fromOffsets $ (scaleY spacing unitY :) $
+--     (replicate n (scaleY spacing unitY)))
+--
+-- intervalPoints :: Double -> Int -> [Point V2 Double]
+-- intervalPoints spacing n =
+--   (fromOffsets $ surroundWith (scaleX (spacing / 2) unitX) $
+--     (replicate n (scaleX spacing unitX)))
+--
+-- increasingX :: Double -> [Point V2 Double]
+-- increasingX inc = go 0
+--   where
+--   go n = scaleX n unitX : go (n + inc)
+--
+-- surroundWith :: a -> [a] -> [a]
+-- surroundWith a as = [a] ++ as ++ [a]
+--
+-- -- Move this into vinyl-plus
+-- -- Can we show that
+-- -- (Rec f (a ': as) -> b) -> f a -> Rec f as -> b
+-- -- its like curry for records
+curryRecFunc :: (Rec f (a ': as) -> b) -> f a -> Rec f as -> b
+curryRecFunc f r rs = f (r :& rs)
+--
+-- renderStackedBarGraph :: StackedBarGraphSettings -> [(String,[Int])] -> Html
+-- renderStackedBarGraph bgs pairs = preEscapedToHtml . renderText
+--   $ renderDia SVG (SVGOptions (mkWidth 400) Nothing "bargraph")
+--   $ buildStackedBarGraph bgs pairs
+--
+-- buildStackedBarGraph :: StackedBarGraphSettings -> [(String,[Int])] -> Diagram B
+-- buildStackedBarGraph bgs pairs =
+--   let maxY = fromMaybe 100 $ maximumMay $ map (sum . snd) pairs
+--       (yInterval, intervalCount) = chooseOptimalInterval maxY
+--       yIntervalHeight = bgsHeight bgs / (fromIntegral intervalCount + 0.3)
+--       scaleUnit i = fromIntegral i * yIntervalHeight / fromIntegral yInterval
+--   in buildYAxis bgs yIntervalHeight intervalCount yInterval # alignBR
+--      <> buildStackedContent bgs scaleUnit pairs # alignL
+--
+-- chooseOptimalInterval :: Int -> (Int,Int)
+-- chooseOptimalInterval maxY =
+--   fromMaybe (1000000,8)
+--   $ fmap (\(interval',ct,_) -> (interval',ct))
+--   $ minimumByMay (\(_,_,a) (_,_,b) -> compare a b)
+--   $ do
+--     x <- acceptableIntervals
+--     y <- acceptableCounts
+--     let diff' = x * y - maxY
+--     guard $ diff' >= 0
+--     return (x,y,diff')
+--   where acceptableIntervals = [1,2,3,4,5,10,20,50,100,200,500,1000,10000,100000]
+--         acceptableCounts = [4,5,6,7]
+--
+--
+-- buildYAxis :: StackedBarGraphSettings -> Double -> Int -> Int -> Diagram B
+-- buildYAxis bgs yIntervalHeight yIntervalCount i =
+--   let nums = reverse $ take yIntervalCount $ map (*i) [1..]
+--       -- shiftCenter x = translate (r2 (0, x))
+--       ticks = foldl' (===) mempty $ flip map nums $ \n ->
+--         hsep 3 [ alignedText 1 0.5 (show n) # fontSizeL (bgsAxisFontSize bgs)
+--                , hrule (bgsTickSize bgs)
+--                ] <> rect (bgsIntervalSpace bgs) yIntervalHeight # lw none # alignTR
+--   in hcat $ map alignB $
+--      [ ticks
+--      , vrule (bgsHeight bgs)
+--      ]
+--
+-- buildStackedContent :: StackedBarGraphSettings -> (Int -> Double) -> [(String,[Int])] -> Diagram B
+-- buildStackedContent bgs scaleUnit pairs =
+--   let pairCount = length pairs
+--       pieceWidth = (bgsWidth bgs) / fromIntegral pairCount
+--       barWidth = pieceWidth * 0.9
+--       infiniteColors = cycle $ bgsColors bgs
+--       mkPiece label vals = mempty
+--         === vcat (reverse $ flip map (zip vals infiniteColors) $ \(val,color) ->
+--                   rect barWidth (scaleUnit val) # fc color # lw none) # alignB
+--         === hrule pieceWidth
+--         === alignedText 0.5 1.0 label # fontSizeL (bgsAxisFontSize bgs)
+--   in hcat (map (uncurry mkPiece) pairs) === rect (bgsAxisFontSize bgs) (bgsAxisFontSize bgs) # lw none
+--
+-- data StackedBarGraphSettings = StackedBarGraphSettings
+--   { bgsColors        :: [Colour Double]
+--   , bgsTickSize      :: Double
+--   , bgsAxisFontSize  :: Double
+--   , bgsHeight        :: Double
+--   , bgsWidth         :: Double
+--   , bgsIntervalSpace :: Double
+--   }
+--
+-- defStackedBarGraphSettings :: StackedBarGraphSettings
+-- defStackedBarGraphSettings = StackedBarGraphSettings
+--   (brewerSet Set1 8)
+--   5 16 200 400 40
 
