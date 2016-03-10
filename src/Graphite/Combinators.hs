8d9755484c9f2203c0a937665a37782c1ce74c63
Andrew Martin <amartin@layer3com.com>
2016-03-10 10:34:11 -0500

    Added more helper functions

--- src/Graphite/Combinators.hs
+++ src/Graphite/Combinators.hs
@@ -33,12 +33,21 @@ figure' f = figure (\_ _ fig1 -> f fig1)
 figureKnownHead :: (x -> fig1 -> fig2) -> Graphite res fig1 (x ': ks) us -> Graphite res fig2 (x ': ks) us
 figureKnownHead f = figure (\(Identity x :& _) _ fig -> f x fig)
 
+figureUnknownHead :: ([x] -> fig1 -> fig2) -> Graphite res fig1 ks (x ': us) -> Graphite res fig2 ks (x ': us)
+figureUnknownHead f = figure (\_ (xs :& _) fig -> f xs fig)
+
 pop :: Graphite res fig (x ': vs) ls -> Graphite res [fig] vs (x ': ls)
 pop (Graphite g) = Graphite $ \vals getVal (valList :& valLists) ->
   flip map valList $ \val -> g 
     (Identity val :& vals) 
     (curryRecFunc getVal (Identity val)) 
     valLists 
+
+popPlus :: Graphite res fig (x ': ks) us -> Graphite res (x -> fig) ks (x ': us)
+popPlus (Graphite g) = Graphite $ \vals getVal (_valList :& valLists) valToCheck ->
+  g (Identity valToCheck :& vals) (curryRecFunc getVal (Identity valToCheck)) valLists 
+
+-- concat :: Graphite res fig1 ks us -> Graphite res fig2 fig1 fig2 
 
 curryRecFunc :: (Rec f (a ': as) -> b) -> f a -> Rec f as -> b
 curryRecFunc f r rs = f (r :& rs)
